3bd6f0d420866e6eadeabb351ecc31f8b64436bd
孟祥臻 <mengxiangzhen.1997@bytedance.com>
2023-11-02 17:42:04 +0800

    增加提交菜单的接口

--- controller/dish.py
+++ controller/dish.py
@@ -1,4 +1,7 @@
+import json
+
 import flask
+from flask import jsonify, request
 from flask_cors import CORS
 
 from app_util import app
@@ -15,9 +18,18 @@ def hello_world():  # put application's code here
     return 'Hello World!'
 
 
-@dish.route('/get_dish')
+@dish.route('/get_dish', methods=['GET'])
 def get_dish():
     dish_query = Dish.query.all()
     res = [item.to_dict() for item in dish_query]
     return res
 
+
+@dish.route('/create_dish', methods=['POST'])
+def create_dish():
+    data = json.loads(request.data)
+    new_dish = Dish()
+    new_dish.name = data['name']
+    db.session.add(new_dish)
+    db.session.commit()
+    return data
